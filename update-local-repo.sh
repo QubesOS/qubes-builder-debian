464b4c77cfc9e5d1910596c7efb1f6da7f3d8748
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2015-04-16 16:08:31 +0200

    Ensure that gnupg-home exists

--- update-local-repo.sh
+++ update-local-repo.sh
@@ -34,6 +34,7 @@ calc_sha1 main/binary-amd64/Packages >> dists/$1/Release.gz
 
 rm -f dists/$DIST/Release.gpg
 export GNUPGHOME=$CACHEDIR/gnupg-home
+mkdir -p $CACHEDIR/gnupg-home
 gpg -abs --no-default-keyring \
     --secret-keyring $KEYS_DIR/repo-secring.gpg \
     --keyring $KEYS_DIR/repo-pubring.gpg \
