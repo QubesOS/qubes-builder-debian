7088c5f4544dd6aac409067ff0c3779a82f66956
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2015-06-10 13:25:07 +0200

    template: remove random-seed
    Prevent all the templates using the same initial random seed.

--- template_debian/09_cleanup.sh
+++ template_debian/09_cleanup.sh
@@ -21,6 +21,8 @@ rm -rf "${INSTALLDIR}/var/cache/apt/pkgcache.bin"
 rm -rf "${INSTALLDIR}/var/cache/apt/srcpkgcache.bin"
 rm -f "${INSTALLDIR}/etc/apt/sources.list.d/qubes-builder.list"
 rm -rf "${INSTALLDIR}/${TMPDIR}"
+rm -f "${INSTALLDIR}/var/lib/systemd/random-seed"
+
 
 # ==============================================================================
 # Execute any template flavor or sub flavor 'post' scripts
