33120e55031033f968bf310a0a9d53be1427c35f
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2015-11-27 23:08:11 +0100

    template: allways execute package installation stage ("02")
    If for example some packages were added, this way it wouldn't be
    neccessary to rebuild the image from scratch. This is mostly for
    development builds.

--- template_debian/02_install_groups.sh
+++ template_debian/02_install_groups.sh
@@ -30,53 +30,50 @@ buildStep "${0}" "pre"
 # Configure base system and install any additional packages which could
 # include +TEMPLATE_FLAVOR such as gnome as set in configuration file
 # ==============================================================================
-if ! [ -f "${INSTALLDIR}/${TMPDIR}/.prepared_groups" ]; then
-    #### '----------------------------------------------------------------------
-    info ' Trap ERR and EXIT signals and cleanup (umount)'
-    #### '----------------------------------------------------------------------
-    trap cleanup ERR
-    trap cleanup EXIT
+#### '----------------------------------------------------------------------
+info ' Trap ERR and EXIT signals and cleanup (umount)'
+#### '----------------------------------------------------------------------
+trap cleanup ERR
+trap cleanup EXIT
 
-    #### '----------------------------------------------------------------------
-    info ' Install standard Debian packages'
-    #### '----------------------------------------------------------------------
-    containsFlavor "minimal" || {
-        if ! [ -f "${INSTALLDIR}/${TMPDIR}/.debian_packages" ]; then
-            packages="$(chroot tasksel --new-install --task-packages standard)"
-            aptInstall ${packages}
-        fi
-    }
-    touch "${INSTALLDIR}/${TMPDIR}/.debian_packages"
+#### '----------------------------------------------------------------------
+info ' Install standard Debian packages'
+#### '----------------------------------------------------------------------
+containsFlavor "minimal" || {
+    if ! [ -f "${INSTALLDIR}/${TMPDIR}/.debian_packages" ]; then
+        packages="$(chroot tasksel --new-install --task-packages standard)"
+        aptInstall ${packages}
+    fi
+}
 
-    #### '----------------------------------------------------------------------
-    info ' Distribution specific steps (install systemd, add sources, etc)'
-    #### '----------------------------------------------------------------------
-    buildStep "$0" "${DIST}"
+#### '----------------------------------------------------------------------
+info ' Distribution specific steps (install systemd, add sources, etc)'
+#### '----------------------------------------------------------------------
+buildStep "$0" "${DIST}"
 
-    #### '----------------------------------------------------------------------
-    info " Installing extra packages in script_${DIST}/packages.list file"
-    #### '----------------------------------------------------------------------
-    installPackages
-    createSnapshot "packages"
-    touch "${INSTALLDIR}/${TMPDIR}/.prepared_packages"
+#### '----------------------------------------------------------------------
+info " Installing extra packages in script_${DIST}/packages.list file"
+#### '----------------------------------------------------------------------
+installPackages
+createSnapshot "packages"
+touch "${INSTALLDIR}/${TMPDIR}/.prepared_packages"
 
-    #### '----------------------------------------------------------------------
-    info ' Execute any template flavor or sub flavor scripts after packages are installed'
-    #### '----------------------------------------------------------------------
-    buildStep "$0" "packages_installed"
+#### '----------------------------------------------------------------------
+info ' Execute any template flavor or sub flavor scripts after packages are installed'
+#### '----------------------------------------------------------------------
+buildStep "$0" "packages_installed"
 
-    #### '----------------------------------------------------------------------
-    info ' apt-get dist-upgrade'
-    #### '----------------------------------------------------------------------
-    aptDistUpgrade
+#### '----------------------------------------------------------------------
+info ' apt-get dist-upgrade'
+#### '----------------------------------------------------------------------
+aptDistUpgrade
 
-    #### '----------------------------------------------------------------------
-    info ' Cleanup'
-    #### '----------------------------------------------------------------------
-    touch "${INSTALLDIR}/${TMPDIR}/.prepared_groups"
-    trap - ERR EXIT
-    trap
-fi
+#### '----------------------------------------------------------------------
+info ' Cleanup'
+#### '----------------------------------------------------------------------
+touch "${INSTALLDIR}/${TMPDIR}/.prepared_groups"
+trap - ERR EXIT
+trap
 
 # ==============================================================================
 # Execute any template flavor or sub flavor 'post' scripts
