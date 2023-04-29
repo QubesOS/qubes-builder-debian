f5d22ba3927ecc8da61d5355d76ee4068ebe1091
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2023-04-29 11:32:50 +0200

    template: do not install firmware-intelwimax
    It is removed in bookworm and according to changelog, it isn't supported
    since 5.10 anyway.

--- template_debian/firmware/02_install_groups_packages_installed.sh
+++ template_debian/firmware/02_install_groups_packages_installed.sh
@@ -10,5 +10,5 @@ source "${TEMPLATE_CONTENT_DIR}/distribution.sh"
 info ' Installing firmware'
 #### '----------------------------------------------------------------------
 chroot_cmd sh -c 'echo "firmware-ipw2x00 firmware-ipw2x00/license/accepted select true" |debconf-set-selections'
-read -r -a packages <<<"atmel-firmware firmware-ath9k-htc firmware-atheros firmware-brcm80211 firmware-ipw2x00 firmware-intelwimax firmware-iwlwifi firmware-misc-nonfree firmware-ralink firmware-realtek firmware-zd1211"
+read -r -a packages <<<"atmel-firmware firmware-ath9k-htc firmware-atheros firmware-brcm80211 firmware-ipw2x00 firmware-iwlwifi firmware-misc-nonfree firmware-ralink firmware-realtek firmware-zd1211"
 aptInstall "${packages[@]}"
