QubesOS/qubes-builder-debian

commit bbe7fa26bfcb4833831ba0bddd9e001982ab87fb

Fix initramfs generation with MODULES=dep

template_debian/09_cleanup.sh
@@ -30,7 +30,8 @@ rm -f "${INSTALL_DIR}/etc/apt/sources.list.d/qubes-builder.list"
 rm -rf "${INSTALL_DIR}/${TMPDIR:?}"
 rm -f "${INSTALL_DIR}/var/lib/systemd/random-seed"
 sed -i "s/$(hostname)/${DIST_NAME}/"  "${INSTALL_DIR}/etc/hosts" || true
-
+rm -rf "${INSTALL_DIR}/etc/initramfs/conf.d/99-template-build.conf" \
+    "${INSTALL_DIR}/usr/share/initramfs-tools/modules.d/qubes-template-build"
 # ==============================================================================
 # Execute any template flavor or sub flavor 'post' scripts
 # ==============================================================================

template_debian/04_install_qubes.sh
@@ -60,6 +60,18 @@ if ! [ -f "${INSTALL_DIR}/${TMPDIR}/.prepared_qubes" ]; then
         #### '------------------------------------------------------------------
         info ' Install kernel and bootloader'
         #### '------------------------------------------------------------------
+        # We have MODULES=dep in qubes conf for initramfs. It does not detect the block
+        # device for /. For template build, this is loop device with chroot into. It would
+        # never find necessary modules to be added.
+        cat > "${INSTALL_DIR}/usr/share/initramfs-tools/modules.d/qubes-template-build" << EOF
+xen-blkfront
+dm-mod
+dm-thin-pool
+dm-persistent-data
+ext4
+EOF
+        aptInstall initramfs-tools
+        echo MODULES=list > "${INSTALL_DIR}/etc/initramfs-tools/conf.d/99-template-build.conf"
         aptInstall qubes-kernel-vm-support
         aptInstall "${KERNEL_PACKAGE_NAME}"
         aptInstall grub-pc

commit 83fbd33187783994bf3ea982d96fecebc87af188

template: fix cleanup of initramfs setup
Fix removing temporary "MODULES=list" initramfs option. Otherwise it
will keep instructing initramfs-tools to include only explicitly listed
modules, while the actual modules list will be gone - resulting in a
broken initramfs.

While at it, remove spurious -r

Fixes: bbe7fa2 "Fix initramfs generation with MODULES=dep"

template_debian/09_cleanup.sh
@@ -30,7 +30,7 @@ rm -f "${INSTALL_DIR}/etc/apt/sources.list.d/qubes-builder.list"
 rm -rf "${INSTALL_DIR}/${TMPDIR:?}"
 rm -f "${INSTALL_DIR}/var/lib/systemd/random-seed"
 sed -i "s/$(hostname)/${DIST_NAME}/"  "${INSTALL_DIR}/etc/hosts" || true
-rm -rf "${INSTALL_DIR}/etc/initramfs/conf.d/99-template-build.conf" \
+rm -f "${INSTALL_DIR}/etc/initramfs-tools/conf.d/99-template-build.conf" \
     "${INSTALL_DIR}/usr/share/initramfs-tools/modules.d/qubes-template-build"
 # ==============================================================================
 # Execute any template flavor or sub flavor 'post' scripts